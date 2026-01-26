ee1563916dd83a211258fc210b1bc1bd4678b05e
rhaynel19 <businessftm10@gmail.com>
2026-01-26 10:23:06 -0400

    feat: mejoras UX, validaciones fiscales y ajustes de landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
+import "./globals.css";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { PreferencesProvider } from "@/components/providers/PreferencesContext";
